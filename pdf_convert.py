17ee049eeb8f62f7c78d1f1830eb9c662294337a
Allen, Timothy <tallen@wharton.upenn.edu>
2015-06-30 06:18:42 -0700

    CSS for PDF creation so it isn't so huge. Rerun PDF creator.

--- pdf_convert.py
+++ pdf_convert.py
@@ -7,4 +7,16 @@ output_filename = 'README.pdf'
 with open(input_filename, 'r') as f:
     html_text = markdown(f.read(), output_format='html4')
 
-pdfkit.from_string(html_text, output_filename)
+options = {
+    'page-size': 'Letter',
+    'margin-top': '0.25in',
+    'margin-right': '0.25in',
+    'margin-bottom': '0.25in',
+    'margin-left': '0.25in',
+    'encoding': "UTF-8",
+    'no-outline': None
+}
+
+css = 'style.css'
+
+pdfkit.from_string(html_text, output_filename, options=options, css=css)
